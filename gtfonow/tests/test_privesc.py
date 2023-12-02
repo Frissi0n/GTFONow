b7489e48e4ba503ddb7ef2cade3ba7e05312e14a
frissi0n <frissi0n@protonmail.ch>
2023-12-02 13:05:32 +0000

    Github actions debugging

--- gtfonow/tests/test_privesc.py
+++ gtfonow/tests/test_privesc.py
@@ -18,12 +18,14 @@ def test_check_suid_bins():
 
 def test_check_sudo_nopasswd_binaries():
     sudo_l_output = get_sudo_l_output()
+    print(sudo_l_output)
     res = check_sudo_nopasswd_binaries(sudo_l_output)
     expected = {
         "Binary": "head",
         "Path": "/usr/bin/head",
         "Payloads": sudo_bins["head"],
-        "Type": "Sudo NOPASSWD",
-        "SudoUser": "root"
+        "SudoUser": "root",
+        "Type": "Sudo NOPASSWD"
+
     }
     assert expected in res
